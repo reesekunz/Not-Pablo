9ac80c7e71f15751b0982b5fb48b5a09349bc614
Reese Kunz <reesekunz21@gmail.com>
2020-02-01 23:43:31 -0700

    finished mvp

--- pablo/src/components/Footer/Contact/Contact.js
+++ pablo/src/components/Footer/Contact/Contact.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Contact.scss";
 import { NavLink } from "react-router-dom";
+import ContactForm from "./ContactForm";
 
 function Contact() {
   return (
     <div className="contact-container">
-      Contact Form Here
+      <ContactForm />
       <div className="other-pages-linkz">
         <NavLink to="/" activeClassName="activeNavButton" className="next-link">
           Back Home
